d133d01523a489a61006a06b4f011451d4e98702
Brad Akemi <mirado.akemi@gmail.com>
2024-07-08 11:55:36 +0300

    modif docker

--- ReservationHotel/settings.py
+++ ReservationHotel/settings.py
@@ -71,24 +71,23 @@ WSGI_APPLICATION = 'ReservationHotel.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'aftrip',
+        'USER': 'postgres',
+        'PASSWORD': 'H{S[jcJ7(x50LD2N',
+        'HOST': '34.122.112.53',
+        'PORT': '5432',
+    }
+}
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'Hotel',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Ars1ne',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
