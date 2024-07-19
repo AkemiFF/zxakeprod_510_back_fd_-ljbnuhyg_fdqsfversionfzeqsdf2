8e8f1aa66b74dde4ed61c4b3bf0c4bc28c994607
Brad Akemi <mirado.akemi@gmail.com>
2024-07-19 18:32:00 +0300

    d

--- Hebergement/serializers.py
+++ Hebergement/serializers.py
@@ -95,7 +95,7 @@ class ChambreSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Chambre
-        fields = "__all__"
+        fields = "images_chambre"
 
 
 class ChambrePersonaliserSerializer(serializers.ModelSerializer):
