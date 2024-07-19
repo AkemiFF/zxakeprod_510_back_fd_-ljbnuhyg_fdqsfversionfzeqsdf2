e2a807a884c36684910f77fdb4bb479953206dbe
NY aro <nyari15400@gmail.com>
2024-07-19 07:30:25 +0200

    modification messages

--- Message/models.py
+++ Message/models.py
@@ -6,16 +6,20 @@ from django.dispatch import receiver
 from Artisanal.models import *
 from TourOperateur.models import *
 from Hebergement.models import *
+from django.conf import settings
 
-class Message(PolymorphicModel):
-    sender = models.ForeignKey(
-        Client, related_name='sent_messages', on_delete=models.CASCADE)
-    subject = models.CharField(max_length=255)
+
+
+class Message(models.Model):
+    client = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='sent_messages', on_delete=models.CASCADE)  # Utilisation de settings.AUTH_USER_MODEL
     content = models.TextField()
+    subject = models.CharField(max_length=255, default='', null=True,blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
-
+    client_is_sender = models.BooleanField(default=False)
+    
     def __str__(self):
-        return f"{self.sender} -> {self.get_receiver()}: {self.subject}"
+        return f"{self.client} -> {self.get_receiver()}: {self.content}"
 
     def get_receiver(self):
         raise NotImplementedError("Subclasses of Message must provide a get_receiver() method.")
@@ -28,39 +32,48 @@ class Message(PolymorphicModel):
 class HebergementMessage(Message):
     receiver = models.ForeignKey(
         Hebergement, related_name='messages', on_delete=models.CASCADE)
-
+    
     def __str__(self):
-        return f"{self.sender} -> {self.receiver}: {self.subject}"
+        if self.client_is_sender:
+            return f"{self.client} -> {self.receiver}: {self.content}"
+        else:
+            return f"{self.receiver} -> {self.client}: {self.content}"
 
     def get_receiver(self):
         return self.receiver
 
 
 class ArtisanatMessage(Message):
-    artisanat = models.ForeignKey(
+    receiver = models.ForeignKey(
         Artisanat, related_name='messages', on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.sender} -> {self.artisanat}: {self.subject}"
+        if self.client_is_sender:
+            return f"{self.client} -> {self.receiver}: {self.content}"
+        else:
+            return f"{self.receiver} -> {self.client}: {self.content}"
 
     def get_receiver(self):
-        return self.artisanat
+        return self.receiver
 
 
 class TourOperateurMessage(Message):
-    tour_operateur = models.ForeignKey(
+    receiver = models.ForeignKey(
         TourOperateur, related_name='messages', on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.sender} -> {self.tour_operateur}: {self.subject}"
+        if self.client_is_sender:
+            return f"{self.client} -> {self.receiver}: {self.content}"
+        else:
+            return f"{self.receiver} -> {self.client}: {self.content}"
 
     def get_receiver(self):
-        return self.tour_operateur
+        return self.receiver
 
 
 @receiver(pre_save, sender=HebergementMessage)
-def check_reservation_before_save(sender, instance, **kwargs):
-    client = instance.sender
+def check_reservation_before_save(sender, instance, **kwargs): 
+    client = instance.client
     hebergement = instance.receiver
 
     if not Reservation.objects.filter(client_reserve=client, hotel_reserve=hebergement).exists():
