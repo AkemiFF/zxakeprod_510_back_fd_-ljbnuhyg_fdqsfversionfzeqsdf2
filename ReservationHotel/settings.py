478bb5e799cc5f01ace47fc99e761afdc367990b
NY aro <nyari15400@gmail.com>
2024-07-30 09:46:00 +0200

    add staticfiles

--- ReservationHotel/settings.py
+++ ReservationHotel/settings.py
@@ -1,6 +1,7 @@
 from decouple import config
 from datetime import timedelta
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -153,6 +154,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
