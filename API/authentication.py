eb05f5d844cc89e39124a638e43eea812ce1656f
Brad Akemi <mirado.akemi@gmail.com>
2024-12-17 11:02:58 +0300

    permission

--- API/authentication.py
+++ API/authentication.py
@@ -1,13 +1,15 @@
+from Accounts.models import Client, ResponsableEtablissement
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.exceptions import InvalidToken
-from rest_framework.exceptions import AuthenticationFailed
-from Accounts.models import Client, ResponsableEtablissement
 
 
 class CustomJWTAuthentication(JWTAuthentication):
     def get_user(self, validated_token):
         try:
-            user = Client.objects.get(id=validated_token["user_id"])
+            user = ResponsableEtablissement.objects.get(
+                    id=validated_token["user_id"]
+                )
         except Client.DoesNotExist:
             try:
                 user = ResponsableEtablissement.objects.get(
